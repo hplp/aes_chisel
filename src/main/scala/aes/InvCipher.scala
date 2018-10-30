d4aefe53a7c036fb2c1e2627c35c195c30d5dbd0
Sergiu Mosanu <sm7ed@virginia.edu>
2018-10-30 12:34:27 -0400

    add InvCipher 128 192 256 module and test

--- src/main/scala/aes/InvCipher.scala
+++ src/main/scala/aes/InvCipher.scala
@@ -4,10 +4,16 @@ import chisel3._
 import chisel3.util._
 
 // implements AES_Decrypt
-class InvCipher extends Module {
+// change Nk=4 for AES128, NK=6 for AES192, Nk=8 for AES256
+class InvCipher(Nk: Int) extends Module {
+  require(Nk == 4 || Nk == 6 || Nk == 8)
+  val KeyLength = Nk * Params.rows
+  val Nr = Nk + 6 // 10, 12, 14 rounds
+  val Nrplus1 = Nr + 1 // 10+1, 12+1, 14+1
+
   val io = IO(new Bundle {
     val ciphertext = Input(Vec(Params.StateLength, UInt(8.W)))
-    val expandedKey = Input(Vec(Params.Nrplus1, Vec(Params.StateLength, UInt(8.W))))
+    val expandedKey = Input(Vec(Nrplus1, Vec(Params.StateLength, UInt(8.W))))
     val start = Input(Bool())
     val state_out = Output(Vec(Params.StateLength, UInt(8.W)))
     val state_out_valid = Output(Bool())
@@ -43,7 +49,7 @@ class InvCipher extends Module {
     }
     is(sBusy) {
       rounds := rounds + 1.U
-      when(rounds === Params.Nr.U) {
+      when(rounds === Nr.U) {
         STM := sIdle
       }
     }
@@ -57,15 +63,15 @@ class InvCipher extends Module {
 
   // AddRoundKey state
   AddRoundKeyModule.io.state_in := Mux(STM === sInitialAR, io.ciphertext, InvSubBytesModule.io.state_out)
-  AddRoundKeyModule.io.roundKey := io.expandedKey(Params.Nr.U - rounds)
+  AddRoundKeyModule.io.roundKey := io.expandedKey(Nr.U - rounds)
 
   // InvMixColumns state
   InvMixColumnsModule.io.state_in := AddRoundKeyModule.io.state_out
 
-  state := Mux(STM =/= sIdle, Mux((rounds > 0.U) & (rounds < Params.Nr.U), InvMixColumnsModule.io.state_out, AddRoundKeyModule.io.state_out), VecInit(initValues))
+  state := Mux(STM =/= sIdle, Mux((rounds > 0.U) & (rounds < Nr.U), InvMixColumnsModule.io.state_out, AddRoundKeyModule.io.state_out), VecInit(initValues))
 
   // Set state_out_valid true when cipher ends
-  io.state_out_valid := rounds === Params.Nrplus1.U
+  io.state_out_valid := rounds === Nrplus1.U
   io.state_out := state
 
   // Debug statements
@@ -75,5 +81,5 @@ class InvCipher extends Module {
 }
 
 object InvCipher {
-  def apply(): InvCipher = Module(new InvCipher())
+  def apply(): InvCipher = Module(new InvCipher(4))
 }
