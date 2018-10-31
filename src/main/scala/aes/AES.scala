d1daef3fd3b4a816e705d33c38de1fb36a4e543f
Sergiu Mosanu <sm7ed@virginia.edu>
2018-10-31 14:57:13 -0400

    extend AES Top with 128 192 256 key size

--- src/main/scala/aes/AES.scala
+++ src/main/scala/aes/AES.scala
@@ -4,27 +4,35 @@ import chisel3._
 import chisel3.util._
 
 // implements wrapper for AES cipher and inverse cipher
-class AES extends Module {
+// change Nk=4 for AES128, NK=6 for AES192, Nk=8 for AES256
+class AES(Nk: Int) extends Module {
+  require(Nk == 4 || Nk == 6 || Nk == 8)
+  val KeyLength = Nk * Params.rows
+  val Nr = Nk + 6 // 10, 12, 14 rounds
+  val Nrplus1 = Nr + 1 // 10+1, 12+1, 14+1
+
   val io = IO(new Bundle {
     val AES_mode = Input(Bool()) // 00=cipher, 01=inverse cipher, later version: 10=key update
     val start = Input(Bool())
     //
     val input_text = Input(Vec(Params.StateLength, UInt(8.W))) // plaintext, ciphertext or expanded key
-    val expandedKey = Input(Vec(Params.Nrplus1, Vec(Params.StateLength, UInt(8.W)))) // for now, send the expanded key
+    val expandedKey = Input(Vec(Nrplus1, Vec(Params.StateLength, UInt(8.W)))) // for now, send the expanded key
     //
     val output_text = Output(Vec(Params.StateLength, UInt(8.W))) // ciphertext or plaintext
     val output_valid = Output(Bool())
   })
 
   // Instantiate module objects
-  val CipherModule = Cipher()
-  val InvCipherModule = InvCipher()
+  val CipherModule = Cipher(Nk)
+  val InvCipherModule = InvCipher(Nk)
+  //  val CipherModule = new Cipher(Nk)
+  //  val InvCipherModule = new InvCipher(Nk)
 
   // Internal variables
   val initValues = Seq.fill(Params.StateLength) {
     0.U(8.W)
   }
-  val output_valid = RegInit(Vec(initValues))
+  val output_valid = RegInit(VecInit(initValues))
 
   // The input text can go to both the cipher and the inverse cipher (for now)
   CipherModule.io.plaintext <> io.input_text
