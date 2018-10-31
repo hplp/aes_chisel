58bafc876ee7991f941a481055f3603f7b41eda4
Sergiu Mosanu <sm7ed@virginia.edu>
2018-10-31 14:49:34 -0400

    remove Nk params

--- src/main/scala/aes/Params.scala
+++ src/main/scala/aes/Params.scala
@@ -4,9 +4,4 @@ object Params {
   val Nb = 4 // columns in state (in this standard)
   val rows = 4
   val StateLength = Nb * rows
-  // change Nk=4 for AES128, NK=6 for AES192, Nk=8 for AES256
-  val Nk = 4 // 4, 6, 8 [32-bit words] columns in cipher key
-  val KeyLength = Nk * rows
-  val Nr = Nk + 6 // 10, 12, 14 rounds
-  val Nrplus1 = Nr + 1 // 10+1, 12+1, 14+1
 }
